2106148a8362f7b11cc02855233ad449530e75bd
DigitalSkillsHouse <digitalskillshouse@gmail.com>
2025-09-22 12:18:47 +0500

    update adds feature

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://bizbranches.pk",
+    generateRobotsTxt: true,
+    outDir: "./public" // ensures files are generated inside /public
+  };
+  
